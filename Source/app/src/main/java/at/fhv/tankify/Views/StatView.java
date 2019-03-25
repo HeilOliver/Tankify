a57ba35b08910699732cba5c3353e53f5bdb766e
Robert Schmitzer <rsc7457@students.fhv.at>
2019-03-25 11:35:49 +0100

    Added StatView
    Co-Authored-By: sge <sge5641@users.noreply.github.com>

---
+++ Source/app/src/main/java/at/fhv/tankify/Views/StatView.java
@@ -0,0 +1,15 @@
+package at.fhv.tankify.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import at.fhv.tankify.R;
+
+public class StatView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_view);
+    }
+}
