57793a67fe85e23318e9943ecd170237e04aa971
Robert Schmitzer <rsc7457@students.fhv.at>
2019-04-15 11:59:50 +0200

    Fragments error
    Co-Authored-By: sge <sge5641@users.noreply.github.com>

--- Source/app/src/main/java/at/fhv/tankify/MainActivity.java
+++ Source/app/src/main/java/at/fhv/tankify/MainActivity.java
@@ -1,14 +1,21 @@
 package at.fhv.tankify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -20,12 +27,46 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        
+
+        if (savedInstanceState == null) {
+            // TODO: fix Startup Screen
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CarFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_car);
+        }
 
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_car:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CarFragment()).commit();
+                break;
+
+            case R.id.nav_fuelings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FuelingFragment()).commit();
+                break;
+
+            case R.id.nav_statistics:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new StatisticsFragment()).commit();
+                break;
+
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
     @Override
     public void onBackPressed(){
