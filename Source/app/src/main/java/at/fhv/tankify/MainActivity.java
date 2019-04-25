f98a8ea7cfae5d3ec9762cf44ad7e9e31118f095
sge <25079878+sge5641@users.noreply.github.com>
2019-04-25 15:47:07 +0200

    fixed startingBug @fragment_car
    Co-Authored-By: Robert Schmitzer <robertschmitzer@users.noreply.github.com>

--- Source/app/src/main/java/at/fhv/tankify/MainActivity.java
+++ Source/app/src/main/java/at/fhv/tankify/MainActivity.java
@@ -33,14 +33,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-        
 
         if (savedInstanceState == null) {
-            // TODO: fix Startup Screen
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CarFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_car);
         }
-
     }
 
     @Override
