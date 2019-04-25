4133ef3a702d0edc5a269965a113048f9bed8dbe
Robert Schmitzer <rsc7457@students.fhv.at>
2019-04-25 17:57:37 +0200

    Added Fueling
    Co-Authored-By: sge <sge5641@users.noreply.github.com>

--- Source/app/src/main/java/at/fhv/tankify/FuelingFragment.java
+++ Source/app/src/main/java/at/fhv/tankify/FuelingFragment.java
@@ -1,19 +1,44 @@
 package at.fhv.tankify;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FuelingFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fueling, container, false);
+        View view = inflater.inflate(R.layout.fragment_fueling, container, false);
+
+        //Todo: bringin Floating Button to work
+        final Button fbutton = view.findViewById(R.id.addFuelingButton);
+        fbutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,new addFueling());
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 }
---
+++ Source/app/src/main/java/at/fhv/tankify/addFueling.java
@@ -0,0 +1,23 @@
+package at.fhv.tankify;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class addFueling extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_fueling, container, false);
+    }
+}
