6bdea2ef1415cc196880558185975522fd26b456
sge <25079878+sge5641@users.noreply.github.com>
2019-04-29 13:05:55 +0200

    added Geolocation

--- Source/app/src/main/java/at/fhv/tankify/MainActivity.java
+++ Source/app/src/main/java/at/fhv/tankify/MainActivity.java
@@ -5,25 +5,41 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+
+    private String text = "default";
+    private LocationManager locationManager;
+    private LocationListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +61,35 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CarFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_car);
         }
+
+
+        // start Geolocation
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                text = String.valueOf(location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+                //
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                //
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        // end Geolocation
     }
 
     @Override
@@ -66,6 +111,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 // Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
                 notificationDialog();
                 break;
+
+            case R.id.nav_location:
+                getGeolocation();
+                Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+                break;
+
         }
 
         drawer.closeDrawer(GravityCompat.START);
@@ -102,7 +153,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         notificationBuilder.setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL)
                 .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.tankifyicon)
                 .setTicker("TestNotification")
                 //.setPriority(Notification.PRIORITY_MAX)
                 .setContentTitle("sample notification")
@@ -110,4 +161,57 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 .setContentInfo("Information");
         notificationManager.notify(1, notificationBuilder.build());
     }
+
+    // start Geolocation
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 10:
+                getGeolocation();
+                break;
+            default:
+                break;
+        }
+    }
+
+    void getGeolocation() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this,
+                ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                request_permission();
+            }
+        } else {
+            // permission has been granted
+            locationManager.requestLocationUpdates("gps", 5000, 0, listener);
+        }
+    }
+
+    private void request_permission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                ACCESS_COARSE_LOCATION)) {
+
+            Snackbar.make(findViewById(R.id.nav_view), "Location permission is needed because ...",
+                    Snackbar.LENGTH_LONG)
+                    .setAction("retry", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            requestPermissions(new String[]{ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
+                        }
+                    })
+                    .show();
+        } else {
+            // permission has not been granted yet. Request it directly.
+            requestPermissions(new String[]{ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 10);
+        }
+    }
+    // end Geolocation
+
+
 }
